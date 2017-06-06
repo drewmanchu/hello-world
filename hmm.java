ceca3bd3680068acc2bf79b8703c08bd3d5fe461
drewmanchu <dwennermarkowens@gmail.com>
2017-06-06 09:32:12 -0700

    Add files via upload

--- hmm.java
+++ hmm.java
@@ -2,6 +2,9 @@ import java.util.*;
 import java.io.*;
 
 public class hmm {
+   private Map<String, Map<String, Double>> tranMaps;
+   private Map<String, Double> emisMap;
+   
    TransitionDistributions DistsPOSPOS = new TransitionDistributions(); 
       //read DistsPOSPOS as "Distributions over POS given a POS"
       
@@ -10,8 +13,9 @@ public class hmm {
 
    public ArrayList<String> tag(ArrayList<String> sentence) {
       ArrayList<String> tags = new ArrayList<String>();
-   
+      matrix hmmMatrix = new matrix(sentence, this.tranMaps, this.emisMap);
+      tags = hmmMatrix.getTagged();
       return tags;
-   }      
+   }    
 
 }
