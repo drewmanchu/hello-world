b2d2f2aff3bbd91a0113f0bb7da8a1ecd7ed2587
drewmanchu <dwennermarkowens@gmail.com>
2017-06-07 11:45:20 -0700

    Add files via upload

--- hmm.java
+++ hmm.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class hmm {
    private Map<String, Map<String, Double>> tranMaps;
-   private Map<String, Double> emisMap;
+   private Map<String, Map<String, Double>> emisMap;
    
    TransitionDistributions DistsPOSPOS = new TransitionDistributions(); 
       //read DistsPOSPOS as "Distributions over POS given a POS"
@@ -11,11 +11,20 @@ public class hmm {
    EmissionsDistributions DistsWordPOS = new EmissionsDistributions(); 
       //read DistsWordPOS as "Distributions over Words given a POS"
 
+   // Returns all POS tags. Important for confusion matrix interpretation.
+   public ArrayList<String> tagArray(){
+      ArrayList<String> array = new ArrayList<String>();
+      for (String tag : this.tranMaps.keySet()){
+         array.add(tag);
+      }
+      array.add("UNK");
+      return array;
+   }
+   
    public ArrayList<String> tag(ArrayList<String> sentence) {
       ArrayList<String> tags = new ArrayList<String>();
-      matrix hmmMatrix = new matrix(sentence, this.tranMaps, this.emisMap);
+      Matrix hmmMatrix = new Matrix(sentence, this.tranMaps, this.emisMap);
       tags = hmmMatrix.getTagged();
       return tags;
-   }    
-
+   }
 }
